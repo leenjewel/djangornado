b5836e442d1b5cebe24e26f0263bff4fa1485ade
leenjewel <leenjewel@gmail.com>
2010-07-31 17:25:03 +0800

    优化安装脚本

--- setup.py
+++ setup.py
@@ -4,6 +4,23 @@ from distutils.command.install import INSTALL_SCHEMES
 import os
 import sys
 
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': osx_install_data}
+else:
+    cmdclasses = {'install_data': install_data}
 
 def fullsplit(path, result=None):
     """
@@ -19,6 +36,9 @@ def fullsplit(path, result=None):
         return result
     return fullsplit(head, [tail] + result)
 
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
 
@@ -28,9 +48,9 @@ packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir != '':
     os.chdir(root_dir)
-rkornado_dir = 'djangornado'
+django_dir = 'djangornado'
 
-for dirpath, dirnames, filenames in os.walk(rkornado_dir):
+for dirpath, dirnames, filenames in os.walk(django_dir):
     # Ignore dirnames that start with '.'
     for i, dirname in enumerate(dirnames):
         if dirname.startswith('.'): del dirnames[i]
@@ -40,11 +60,12 @@ for dirpath, dirnames, filenames in os.walk(rkornado_dir):
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 # Small hack for working with bdist_wininst.
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
 if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
     for file_info in data_files:
         file_info[0] = '\\PURELIB\\%s' % file_info[0]
 
-# Dynamically calculate the version based on rkornado.VERSION.
+# Dynamically calculate the version based on django.VERSION.
 version = __import__('djangornado').get_version()
 if u'SVN' in version:
     version = ' '.join(version.split(' ')[:-1])
@@ -53,12 +74,11 @@ setup(
     name = "Djangornado",
     version = version.replace(' ', '-'),
     url = 'http://src.rekoo.com/',
-    download_url = '',
-    author = 'leenjewel',
+    author = 'Leen Jewel',
     author_email = 'leenjewel@gmail.com',
-    description = 'Extension for tornado',
+    description = 'Extension for tornado.',
     packages = packages,
+    cmdclass = cmdclasses,
     data_files = data_files,
-    script = ["djangornado/bin/djangornado-amin.py"],
+    scripts = ['djangornado/bin/djangornado-admin.py'],
 )
-
