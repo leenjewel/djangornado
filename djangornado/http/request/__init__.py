2cdef43fcf88af6664068bc460cfeb653eeb077f
leenjewel <leenjewel@gmail.com>
2011-09-15 17:57:45 +0800

    Add some dict method for request object.

--- djangornado/http/request/__init__.py
+++ djangornado/http/request/__init__.py
@@ -7,6 +7,7 @@ class DjangornadoRequest(object):
     
     def __init__(self, handler, path, *args, **kwargs):
         self.handler = handler
+        self._request_arguments = None
         if path.startswith("/") is False:
             self.path = "/" + path
         else:
@@ -15,6 +16,17 @@ class DjangornadoRequest(object):
             self.path = args[0]
         except:
             pass
+
+    def _init_request_arguments(self):
+        if (self._request_arguments is None):
+            self._request_arguments = {}
+            for k in self.handler.request.arguments.keys():
+                v = self.handler.get_arguments(k)
+                if len(v) == 1:
+                    v = v[-1]
+                self._request_arguments[k] = v
+        return self._request_arguments
+    request_arguments = property(_init_request_arguments)
     
     def __getattr__(self, attr):
         if hasattr(self.handler, attr):
@@ -26,6 +38,18 @@ class DjangornadoRequest(object):
     def get(self, key, default = None):
         return self.handler.get_argument(key, default)
     
+    def items(self):
+        return self.request_arguments.items()
+
+    def keys(self):
+        return self.handler.request.arguments.keys()
+
+    def values(self):
+        return self.handler.request.arguments.values()
+
+    def __getitem__(self, key):
+        return self.request_arguments[key]
+
     def getlist(self, key, strip=True):
         return self.handler.get_arguments(key, strip)
 
